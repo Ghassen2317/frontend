26a6b5bd2c7481f17ad69b4fa27a6825021db0e1
Ghassen <benghazighassen12@gmail.com>
2023-04-28 13:41:13 +0100

    template frontend

---
+++ src/examples/Footer/index.js
@@ -0,0 +1,41 @@
+// @mui material components
+import Icon from "@mui/material/Icon";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+
+// Material Dashboard 2 React base styles
+import typography from "assets/theme/base/typography";
+
+function Footer() {
+  const { size } = typography;
+  return (
+    <MDBox
+      width="100%"
+      display="flex"
+      flexDirection={{ xs: "column", lg: "row" }}
+      justifyContent="space-between"
+      alignItems="center"
+      px={1.5}
+    >
+      <MDBox
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexWrap="wrap"
+        color="text"
+        fontSize={size.sm}
+        px={1.5}
+      >
+        &copy; {new Date().getFullYear()}, made with
+        <MDBox fontSize={size.md} color="text" mb={-0.5} mx={0.25}>
+          <Icon color="inherit" fontSize="inherit">
+            favorite
+          </Icon>
+        </MDBox>
+      </MDBox>
+    </MDBox>
+  );
+}
+
+export default Footer;
